b1f73cfccd73f879f688a683ccd6d9ba3d2a261b
Dan Hellem <dan_hellem@hotmail.com>
2023-06-09 09:08:31 -0700

    test

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,26 @@
 import * as core from '@actions/core'
 import {wait} from './wait'
 
-async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+// async function run(): Promise<void> {
+//   try {
+//     const ms: string = core.getInput('milliseconds')
+//     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+//     core.debug(new Date().toTimeString())
+//     await wait(parseInt(ms, 10))
+//     core.debug(new Date().toTimeString())
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
+//     core.setOutput('time', new Date().toTimeString())
+//   } catch (error) {
+//     if (error instanceof Error) core.setFailed(error.message)
+//   }
+// }
+
+async function run() {
+  try { 
+     return 1; 
+  } 
+  catch (error) {    
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
