935ca20c21a3641f82dd680ad8a65d0e35da716c
Dan Hellem <dan_hellem@hotmail.com>
2023-06-19 16:44:59 -0400

    modifcations

--- dist/index.js
+++ dist/index.js
@@ -66,16 +66,19 @@ function run() {
             var work_item_id = '';
             const octokit = github.getOctokit(github_token);
             // check if pull request description contains a AB#<work item number>
-            console.log("Checking to see if text 'AB#<work item id>' is contained in pull request description...");
+            console.log("Checking to see if text 'AB#<work item id>' is contained in pull request...");
             if (ab_lookup_match && ab_lookup_match.length > 1) {
                 work_item_id = ab_lookup_match[1].toString();
-                console.log("  AB#" + work_item_id + " found in pull request description.");
-                console.log("Check to see if Bot created link from AB#" + work_item_id + " ...");
+                console.log("AB#" + work_item_id + " found in pull request description.");
+                console.log("Checking to see if bot created link from AB#" + work_item_id + " ...");
                 if ((pull_request_description === null || pull_request_description === void 0 ? void 0 : pull_request_description.includes('[AB#')) && (pull_request_description === null || pull_request_description === void 0 ? void 0 : pull_request_description.includes('/_workitems/edit/'))) {
-                    console.log("  AB#" + work_item_id + " link found.");
-                    console.log("  Logging message in pull request comment and exit routine.");
+                    console.log("AB#" + work_item_id + " link found.");
+                    console.log("Logging message in pull request comment and exit routine.");
                     yield octokit.rest.issues.createComment(Object.assign(Object.assign({}, context.repo), { issue_number: pull_request_number, body: 'Pull request description contains link AB#' + work_item_id + ' to an Azure Boards work item.' }));
                     return;
+                }
+                else {
+                    console.log("Bot did not successfully create a link from AB#" + work_item_id);
                 }
             }
             else {
