3f66b40b8ef31e5dee28087c983743df3ed5b744
Dan Hellem <dan_hellem@hotmail.com>
2023-07-14 12:18:34 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# 🎬 Action, check for linked Azure DevOps work item
+
+Use this action to check your pull request to make sure it is linked to a work item using ``AB#`` before you can merge.
+
+Click here to see the action in the GitHub marketplace.
+
+## 💁‍♂️ Usage
+
+Make sure the GitHub repository is properly linked to an Azure DevOps project. The following docs will help you get started and setup:
+
+- [Learn about the Azure Boards - GitHub integration](https://learn.microsoft.com/en-us/azure/devops/boards/github/?view=azure-devops)
+- [Install the Azure Boards app](https://github.com/marketplace/azure-boards)
+- [Connect a GitHub repository to Azure Boards](https://learn.microsoft.com/en-us/azure/devops/boards/github/add-remove-repositories?view=azure-devops)
+- [Link GitHub commits and pull requests to work items in Azure Boards](https://learn.microsoft.com/en-us/azure/devops/boards/github/link-to-from-github?view=azure-devops)
+
+## 👩‍🏫 How it works
+
+The action will check the pull request description for ``AB#`` and a valid work item id. If it finds a valid work item id, it will pass. If it does not find a valid work item id, it will fail.
+
+If the sender is ``azure-boards[bot]`` or ``dependabot[bot]`` the action will ignore the check.
+
+## ✅ Codes
+
+We use a series of code values to mark and check previous comments to ensure the actions does not get too chatty after each run. For example, if you don't add ``AB#{ID}`` to the description, we don't need to tell you every time you update the desciption. We will only tell you once untill something changes.
+
+### lcc-200
+
+Successfully found a valid work item id in the pull request description.
+
+### lcc-404
+
+Missing ``AB#`` in the pull request description.
+
+### lcc-416
+
+Found ``AB#`` but the work item id is not valid.
+
+## 📄 validate-pr-ab.yml
+
+This is the main workflow file. It will run on any pull request that is opened, reopened, or edited. It will only run on the ``main`` branch. This file needs to be added to the ``.github/workflows`` folder in your repository.
+
+```yml
+name: 'Description contains AB# with a valid work item id'
+on: # rebuild any PRs for main branch changes
+  pull_request:
+    types: [opened, reopened, edited]
+    branches:
+      - main 
+jobs:
+  create-edit-comment:
+    name: check   
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v3
+      - uses: ./
+```
---
+++ src/main.ts
@@ -0,0 +1,150 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+import {Context} from '@actions/github/lib/context'
+
+async function run(): Promise<void> {
+  try {
+    const context: Context = github.context
+    const github_token: string = core.getInput('repo-token')    
+    const pull_request_number: number = context.payload.pull_request?.number ?? 0    
+    const pull_request_description: string = context.payload.pull_request?.body ?? ''    
+    const ab_lookup_match: RegExpMatchArray | null = pull_request_description.match(/AB#([^ \]]+)/g) 
+    const repository_owner: string = context.payload.repository?.owner.login ?? '' 
+    const repository_name: string = context.payload.repository?.name ?? ''
+    const sender_login: string = context.payload.sender?.login ?? ''
+    
+    let work_item_id = ''
+    let last_comment_posted_by_action = ""    
+
+    const octokit = github.getOctokit(github_token)        
+
+    //console.log(`Repository owner: ${repository_owner}`)
+    //console.log(`Repository name: ${repository_name}`)  
+    //console.log(`Sender login: ${sender_login}`)
+    //console.log(`Event name: ${context.eventName}`)
+    //console.log(`Pull request number: ${pull_request_number}`)   
+    //console.log(`Pull request description: ${pull_request_description}`)
+    //console.log(`Comment: ${context.payload.comment?.body}`)
+
+    // if the sender in the azure-boards bot or dependabot, then exit code
+    // nothing needs to be done
+    if (sender_login === "azure-boards[bot]" || sender_login === "dependabot[bot]") {
+      console.log(`azure-boards[bot] or dependabot[bot] sender, exiting action.`)
+      return
+    }
+
+    if (context.eventName === 'pull_request') {   
+      
+      // get all comments for the pull request
+      try {
+        const response = await octokit.rest.issues.listComments({
+          owner: repository_owner,
+          repo: repository_name,
+          issue_number:  pull_request_number,
+        })
+
+        // check for comments
+        if (response.data.length > 0) {
+          const comments: IComments[] = response.data.map((comment) => {
+            return {
+              id: comment.id, 
+              created_at: new Date(comment.created_at),
+              body: comment.body
+            }
+          })  
+
+          // sort comments by date descending
+          comments.sort((a, b) => b.created_at?.getTime()! - a.created_at?.getTime()!) 
+          
+          // loop through comments and grab the most recent comment posted by this action
+          // we want to use this to check later so we don't post duplicate comments
+          for (const comment of comments) { 
+            if (comment.body?.includes('lcc-404')) { 
+              last_comment_posted_by_action = "lcc-404"
+              break
+            }
+
+            if (comment.body?.includes('lcc-416')) { 
+              last_comment_posted_by_action = "lcc-416"
+              break
+            }        
+            
+            if (comment.body?.includes('lcc-200')) {
+              last_comment_posted_by_action = "lcc-200"
+              break
+            }
+          }          
+        }
+        
+      } catch (error) {
+        console.log(error)
+      }    
+
+      // check if pull request description contains a AB#<work item number>
+      console.log(`Checking description for AB#{ID} ...`)
+     
+      if (ab_lookup_match) {        
+        
+        for (const match of ab_lookup_match) {
+          work_item_id = match.substring(3)
+          break    
+        }
+
+        console.log(`AB#${work_item_id} found in pull request description.`)
+        console.log(`Checking to see if bot created link ...`)    
+          
+        if (pull_request_description?.includes('[AB#') && pull_request_description?.includes('/_workitems/edit/')) {
+          console.log(`Success: AB#${work_item_id} link found.`)
+          console.log('Done.')
+          
+          // if the last comment is the check failed, now it passed and we can post a new comment
+          if (last_comment_posted_by_action !== "lcc-200") { 
+            await octokit.rest.issues.createComment({
+              ...context.repo,
+              issue_number: pull_request_number,
+              body: `Work item link check complete. Description contains link AB#${work_item_id} to an Azure Boards work item. <br><br>*code: lcc-200*`
+            })
+          }
+
+          return
+        }
+        else {
+          console.log(`Bot did not create a link from AB#${work_item_id}`)
+          
+          if (last_comment_posted_by_action !== "lcc-416") {
+            await octokit.rest.issues.createComment({
+              ...context.repo,
+              issue_number: pull_request_number,
+              body: `Work item link check failed. Description contains AB#${work_item_id} but the Bot could not link it to an Azure Boards work item. [Click here](https://learn.microsoft.com/en-us/azure/devops/boards/github/link-to-from-github?view=azure-devops#use-ab-mention-to-link-from-github-to-azure-boards-work-items) to learn more.<br><br>*code: lcc-416*`
+            }) 
+          }
+          
+          core.setFailed(`Description contains AB#${work_item_id} but the Bot could not link it to an Azure Boards work item`)
+        }      
+      }   
+      else {   
+          if (last_comment_posted_by_action !== "lcc-404") {
+            await octokit.rest.issues.createComment({
+              ...context.repo,
+              issue_number: pull_request_number,
+              body: ` Work item link check failed. Description does not contain AB#{ID}. [Click here](https://learn.microsoft.com/en-us/azure/devops/boards/github/link-to-from-github?view=azure-devops#use-ab-mention-to-link-from-github-to-azure-boards-work-items) to Learn more.<br><br>*code: lcc-404*`
+            }) 
+          }
+
+          core.setFailed('Description does not contain AB#{ID}')
+      }    
+    } 
+
+    octokit == null
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
+
+interface IComments {
+  id: Number | undefined,
+  created_at: Date | undefined,
+  body: string | undefined,
+}
+
+run()
